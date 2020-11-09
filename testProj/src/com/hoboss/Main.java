817fa479b221c182528aedef2ff2f0a41436a28e
hobossa <hoboss@gmail.com>
2020-11-10 00:25:07 +0800

    new

---
+++ testProj/src/com/hoboss/Main.java
@@ -0,0 +1,14 @@
+package com.hoboss;
+
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+        // generate some fake data;
+
+        System.out.println("hello");
+
+    }
+
+}
