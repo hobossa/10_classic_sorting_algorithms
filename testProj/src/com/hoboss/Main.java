a2a6e129c8752a5265d057849d5024b8921d6c86
hobossa <hoboss@gmail.com>
2020-11-10 20:01:22 +0800

    none

--- testProj/src/com/hoboss/Main.java
+++ testProj/src/com/hoboss/Main.java
@@ -1,14 +1,23 @@
 package com.hoboss;
 
-import java.util.*;
+import java.util.Arrays;
+
+class MergeSort {
+
+    public static int[] sort(int[] sourceArray) {
+        int[] arr = Arrays.copyOf(sourceArray, sourceArray.length);
+        
+        return arr;
+    }
+
+}
 
 public class Main {
 
     public static void main(String[] args) {
         // generate some fake data;
-
-        System.out.println("hello");
-
+        int[] array = {3, 6, 2, 7, 234, 1, 432, 5, 325, 1234, 13, 41, 36, 23, 123, 32, 412, 4, 1324, 14, 2435};
+        System.out.println(Arrays.toString(MergeSort.sort(array)));
     }
 
 }
